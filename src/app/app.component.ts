01da82cefcbe2dc08c31862a5fa6a728b86d507b
Elias Khan <elias.khan@inetum-realdolmen.world>
2023-03-05 14:03:48 +0100

    change something in .ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prettierprecommittest';
-  text = 'prettierprecommittest';
-  variable = 'prettierprecommittest';
+  user = { name: 'John Doe', age: 30 };
 }
