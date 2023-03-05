bb93f1c08fce12914ec854f5e3c16cd717858d01
Elias Khan <elias.khan@inetum-realdolmen.world>
2023-03-05 14:42:05 +0100

    test another change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'prettierprecommittest';
-  user = {
-    name: 'John Doe',
-    age: 30,
-  };
-  quotes = 'This "example" is single quoted';
+    title = 'prettierprecommittest';
+    user = { name: 'John Doe', age: 30 };
+    quotes = 'This "example" is single quoted';
 }
